7bc3eca32f786d4940b80f4427fb835a053ef601
gulien <j.neuhart@thecodingmachine.com>
2018-09-07 15:19:51 +0200

    Brew formula update for Aenthill version 0.0.21

--- Formulae/aenthill.rb
+++ Formulae/aenthill.rb
@@ -3,9 +3,13 @@ class Aenthill < Formula
   homepage "https://aenthill.github.io/"
   url "https://github.com/aenthill/aenthill/releases/download/0.0.21/aenthill_darwin_amd64.tar.gz"
   version "0.0.21"
-  sha256 "63a8cbccb613ac0a5bfc4e0fe311016df42fbb4a14b3379e4c825e944852cd80"
+  sha256 "5ff1cd05ae356a21eabacdd237e2036e21fb1260a7bb1244c891f76938980a6c"
 
   def install
     bin.install "aenthill"
+  end
+
+  test do
+    system "#{bin}/aenthill --version"
   end
 end

59e2e0c503f20910583b51114c506c3c6e9028f4
gulien <j.neuhart@thecodingmachine.com>
2018-11-22 15:28:09 +0100

    Brew formula update for Aenthill version 0.0.26

--- Formulae/aenthill.rb
+++ Formulae/aenthill.rb
@@ -1,9 +1,9 @@
 class Aenthill < Formula
   desc "command-line tool that helps bootstraping your Docker projects easily"
   homepage "https://aenthill.github.io/"
-  url "https://github.com/aenthill/aenthill/releases/download/0.0.25/aenthill_darwin_amd64.tar.gz"
-  version "0.0.25"
-  sha256 "2d262432617583d663837dc5797bf026a0bd81fe3745b224de1bcfd42afba447"
+  url "https://github.com/aenthill/aenthill/releases/download/0.0.26/aenthill_darwin_amd64.tar.gz"
+  version "0.0.26"
+  sha256 "c2be3fe279871129deab00b95a4080b6044c91150b7644e65e6e641b9c65a884"
 
   def install
     bin.install "aenthill"
